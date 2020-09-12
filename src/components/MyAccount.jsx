89118d643351429eb5382df685c446ef7e853cf6
Alex_Bellamy <alexlbellamy@gmail.com>
2020-09-12 12:15:20 +0200

    formatting my bids containter and bid status elements

--- src/components/MyAccount.jsx
+++ src/components/MyAccount.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Item, Label, Button, Icon, Container, Header, Divider } from "semantic-ui-react";
+import {
+  Item,
+  Label,
+  Button,
+  Icon,
+  Header,
+  Divider,
+} from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 const MyAccount = () => {
   const [myListing, setMyListing] = useState([]);
-  const [myBiddings, setMyBiddings] = useState([])
+  const [myBiddings, setMyBiddings] = useState([]);
 
   useEffect(() => {
     getListing();
-    getBids()
+    getBids();
   }, []);
 
   const getListing = async () => {
@@ -19,42 +26,55 @@ const MyAccount = () => {
   };
 
   const getBids = async () => {
-    
     const headers = JSON.parse(localStorage.getItem("J-tockAuth-Storage"));
     let response = await axios.get("/account/biddings", { headers: headers });
-    setMyBiddings(response.data.biddings)
-  }
+    setMyBiddings(response.data.biddings);
+  };
 
-  let bids
-    
+  let bids;
+
   if (myBiddings.length > 0) {
-    bids = myBiddings.map(mybid => (
-      <Item.Group divided>
-      <Item.Group data-cy='user-bids' >
+    bids = myBiddings.map((mybid) => (
+      <Item.Group data-cy="user-bids">
         <Item data-cy={`bid-${mybid.listing.id}`}>
-          <Item.Image data-cy='listing-image' src={mybid.listing.image} alt="parking" />
+          <Item.Image
+            data-cy="listing-image"
+            src={mybid.listing.image}
+            alt="parking"
+          />
           <Item.Content>
-          <Item.Header data-cy="listing-lead" >{mybid.listing.lead}</Item.Header>
-          <Item.Meta data-cy="listing-category" >{mybid.listing.category}</Item.Meta>
-          <Label data-cy="listing-scene" >{mybid.listing.scene}</Label><br /><br />
-          <Item.Content >
-          <Item.Extra>
-          <Item.Header floated="right" data-cy="bid-status" ><h3>Bid status:</h3>{mybid.status}</Item.Header>
-          <Item.Header data-cy="bid-offer" ><h3>Bid amount:</h3>{mybid.bid}</Item.Header>
-          </Item.Extra>
-          </Item.Content>
+            <Item.Header data-cy="listing-lead">
+              {mybid.listing.lead}
+            </Item.Header>
+            <Item.Meta data-cy="listing-category">
+              {mybid.listing.category}
+            </Item.Meta>
+            <Item.Extra>
+              <Label data-cy="listing-scene">{mybid.listing.scene}</Label>
+              <Button color="orange" floated="right" data-cy="button">
+                <Item.Header data-cy="bid-status">
+                  <h3>Bid status:</h3>
+                  {mybid.status}
+                </Item.Header>
+              </Button>
+              <Button primary floated="right">
+                <Item.Header data-cy="bid-offer">
+                  <h3>Bid amount (SEK):</h3>
+                  {mybid.bid}
+                </Item.Header>
+              </Button>
+            </Item.Extra>
           </Item.Content>
         </Item>
       </Item.Group>
-      </Item.Group>
-    ))
+    ));
   } else {
     bids = (
       <>
-        <h1 data-cy='message'>You have not placed any bids.</h1>
+        <h1 data-cy="message">You have not placed any bids.</h1>
       </>
-    ) 
-  } 
+    );
+  }
 
   let content = myListing.map((listing) => (
     <Item.Group divided>
@@ -84,12 +104,14 @@ const MyAccount = () => {
         My Account
       </Header>
       <Divider section />
-      <Container text>
-      <Header as='h2'id="myaccount">My Bids</Header>
+      <Header as="h2" id="myaccount">
+        My Bids
+      </Header>
       <div>{bids}</div>
-      </Container>
       <Divider section />
-      <Header as='h2'id="myaccount">My Listings</Header>
+      <Header as="h2" id="myaccount">
+        My Listings
+      </Header>
       <div>{content}</div>
     </div>
   );
